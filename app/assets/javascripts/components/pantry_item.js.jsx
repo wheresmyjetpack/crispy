2b77219aa3d8cbec7067ddd8e13a7c7285ab392b
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-12 01:04:15 -0600

    adding PantryItems react component

--- app/assets/javascripts/components/pantry_item.js.jsx
+++
@@ -1,3 +0,0 @@
-<div className="pantry-items">
-  <h2 className="title">Pantry</h2>
-</div>
---
+++ app/assets/javascripts/components/pantry_items.js.jsx
@@ -0,0 +1,9 @@
+var PantryItems = React.createClass({
+  render: function() {
+    return (
+      <div className="pantry-items">
+      <h2 className="title">Pantry</h2>
+      </div>
+    );
+  }
+});
