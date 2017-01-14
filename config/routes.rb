a5db175c1b9a01711ec3b4d5bd9fb76b3b5f3753
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-13 22:03:03 -0600

    adding devise, dotenv-rails gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'pantry-items/', to: 'pantry_items#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pantry_items#index'
+  resources :pantry_items, only: [:index]
 end
