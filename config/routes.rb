d9ee2742613fc9cf89028993091c70815ef1dfcc
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-14 23:32:58 -0600

    switching "_" for "-" in pantry items URI

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'sessions#new'
 
-  get 'pantry_items', to: 'pantry_items#index'
+  get 'pantry-items', to: 'pantry_items#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
