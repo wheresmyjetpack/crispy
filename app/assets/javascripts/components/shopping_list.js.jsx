e9d2fab580aa1ddb1d4a35f570c673359621d910
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-15 16:12:24 -0600

    adding initial attempt at field removal in shopping list form

--- app/assets/javascripts/components/shopping_list.js.jsx
+++ app/assets/javascripts/components/shopping_list.js.jsx
@@ -1,7 +1,9 @@
 class ShoppingList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {inputs: ['input-0', 'input-1', 'input-2']};
+    this.state = {
+      inputs: ['input-0', 'input-1', 'input-2']
+    };
   }
 
   render() {
@@ -9,9 +11,9 @@ class ShoppingList extends React.Component {
     <div className="card w-50 p-4">
       <form role="form" acceptCharset="UTF-8" action="/shopping_list" method="post">
         <input type="hidden" name="authenticity_token" value={this.props.authenticity_token} />
-        { this.state.inputs.map(input => <input name="shopping_list[]" key={input} className="form-control m-2" />) }
+        {this.renderInputs()}
         <hr />
-        <button type="button" className="btn btn-primary m-2" onClick={ () => this.appendInput() }>
+        <button type="button" className="btn btn-primary m-2" onClick={ () => this.appendShoppingItem() }>
           + Item
         </button>
         <input type="submit" className="btn btn-success m-2" />
@@ -20,12 +22,39 @@ class ShoppingList extends React.Component {
     );
   }
 
+
+  renderInputs() {
+    return this.state.inputs.map((inputName, index) => {
+      let divName = 'shoppingItem-' + index;
+      return (
+        <div className="shopping-item" key={divName}>
+          <input name="shopping_list[]" key={inputName} className="form-control my-2" />
+          <button type="button" className="btn btn-danger m-2" onClick={ () => this.removeInput(inputName) }>
+            - Item
+          </button>
+        </div>
+      );
+    });
+  }
+
   appendInput() {
     let newInput = `input-${this.state.inputs.length}`;
     this.setState({ inputs: this.state.inputs.concat([newInput]) });
   }
 
-  removeInput() {
-    
+  appendShoppingItem() {
+    let newDiv = `shopping-item-${this.state.inputs.length}`;
+    this.setState({ shoppingItems: this.state.inputs.concat([newDiv]) });
+    this.appendInput();
+  }
+
+  removeInput(inputName) {
+    this.setState(
+      {
+        inputs: this.state.inputs.filter( (item) => {
+          return item !== inputName;
+        })
+      }
+    );
   }
 }
