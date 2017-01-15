cb9846983bcb8e875a9dbe39fbf1b175e7a99371
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-15 14:22:48 -0600

    adding minor styling touchups

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   def authenticate_user!
-    redirect_to(login_path, notice: 'You must sign in') unless user_signed_in?
+    redirect_to_login unless user_signed_in?
+  end
+
+  def redirect_to_login
+    flash[:warning] = 'Please sign in before continuing'
+    redirect_to(login_path)
   end
 end
