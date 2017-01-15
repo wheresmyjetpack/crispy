f7179008f558c0ae21bee54b80af0dffed25a32c
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-15 14:07:01 -0600

    adding shopping list routes, nav link

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  root 'sessions#new'
+  root 'ingredients#index'
 
-  get 'ingredients', to: 'ingredients#index'
   get 'login', to: 'sessions#new'
+
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+  get 'shopping_list', to: 'shopping_items#show'
+  get 'shopping_list/new', to: 'shopping_items#new'
+  post 'shopping_list', to: 'shopping_items#create'
+
+  get 'ingredients', to: 'ingredients#index'
   resources :users
 end
