451c4105ef023f0917cf47a57af165125d318b21
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-14 23:14:23 -0600

    adding logout and header partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,15 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to current_user unless current_user.nil?
   end
 
   def create
     Sessions::AuthenticateUser.call(email: params[:session][:email], password: params[:session][:password]) do
       on(:ok) do |user| 
-        log_in user
+        log_in(user)
         flash[:success] = "Welcome to the app!"
 
-        redirect_to current_user
+        redirect_to(current_user)
       end
       on(:danger) do
         flash.now[:danger] = 'Invalid email/password'
@@ -18,5 +19,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+
+    redirect_to(root_url)
   end
 end
