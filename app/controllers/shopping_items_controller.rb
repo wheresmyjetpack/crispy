5a308bc828d09980fbe327c6e1d11a0dcc8278c1
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-15 16:11:23 -0600

    listening for broadcast in shopping list controller

--- app/controllers/shopping_items_controller.rb
+++ app/controllers/shopping_items_controller.rb
@@ -9,7 +9,16 @@ class ShoppingItemsController < ApplicationController
   end
 
   def create
-    session[:shopping_list] = ShoppingItems::PrepareShoppingList.call(params[:shopping_list])
-    redirect_to action: :show
+    ShoppingItems::PrepareShoppingList.call(params[:shopping_list]) do
+      on(:ok) do |shopping_list|
+        session[:shopping_list] = shopping_list
+        flash[:success] = 'Shopping list created'
+        redirect_to action: :show
+      end
+      on(:danger) do
+        flash[:danger] = 'Please enter a few items in the shopping list'
+        redirect_to action: :new
+      end
+    end
   end
 end
