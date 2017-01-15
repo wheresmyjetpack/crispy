5cb6a192bb439ae0fa065823b07df06b4a95747d
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-14 22:17:23 -0600

    adding user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, length: { minimum: 6 }
 end
