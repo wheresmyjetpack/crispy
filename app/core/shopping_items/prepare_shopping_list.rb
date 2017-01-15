20fa03629c09ab5f4219d63cbe13c9f110ab944b
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-15 16:11:00 -0600

    checking for empty shopping lists in PrepareShoppingList command

--- app/core/shopping_items/prepare_shopping_list.rb
+++ app/core/shopping_items/prepare_shopping_list.rb
@@ -5,11 +5,19 @@ module ShoppingItems
     end
 
     def call
-      shopping_list
+      if prepared_items.empty?
+        broadcast(:danger)
+      else
+        broadcast(:ok, prepared_items)
+      end
     end
 
     private
 
     attr_reader :shopping_list
+
+    def prepared_items
+      shopping_list.reject(&:blank?)
+    end
   end
 end
