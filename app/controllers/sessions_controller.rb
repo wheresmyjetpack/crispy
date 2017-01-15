066b4c320a34a6b42c044d92e2f1e70bf22938ec
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-14 22:25:55 -0600

    logging user id in session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,12 @@ class SessionsController < ApplicationController
 
   def create
     Sessions::AuthenticateUser.call(email: params[:session][:email], password: params[:session][:password]) do
-      on(:ok) { |user| flash[:success] = "Welcome to the app!" }
+      on(:ok) do |user| 
+        log_in user
+        flash[:success] = "Welcome to the app!"
+
+        redirect_to user
+      end
       on(:danger) do
         flash.now[:danger] = 'Invalid email/password'
         render 'new'
