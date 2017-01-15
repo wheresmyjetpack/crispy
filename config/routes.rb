daca3b2e67dba7a18a1248466dda3c35bafda249
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-14 22:17:53 -0600

    adding login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root 'pantry_items#index'
-  resources :pantry_items, only: [:index]
+
+  get 'pantry_items', to: 'pantry_items#index'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :users
 end
