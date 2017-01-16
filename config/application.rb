cd59f8b965f497b56186806e3c8f3111faabd659
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-15 18:47:07 -0600

    adding js.jsx browserify transformers

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Crispy
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.browserify_rails.commandline_options = "-t reactify --extension=\".js.jsx\""
   end
 end
