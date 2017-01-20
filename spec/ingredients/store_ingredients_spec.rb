6dc248c723dda51043cb96d00f52331d1b7cfb44
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-19 22:02:10 -0600

    cleaning up Ingredients::StoreIngredients spec

--- spec/ingredients/store_ingredients_spec.rb
+++ spec/ingredients/store_ingredients_spec.rb
@@ -4,7 +4,7 @@ require 'core/ingredients/ingredient_form'
 
 describe Ingredients::StoreIngredients do
   let(:form_type) { class_double(Ingredients::IngredientForm, from_params: form_instance) }
-  let(:form_instance) { instance_double(Ingredients::IngredientForm, name: nil) }
+  let(:form_instance) { instance_double(Ingredients::IngredientForm, name: 'ingredient name') }
   let(:record) { double('record', save!: nil) }
   let(:relation) { double('relation', new: record) }
 
@@ -16,17 +16,17 @@ describe Ingredients::StoreIngredients do
 
       describe 'the form' do
         subject { form_type }
-        it { is_expected.to have_received(:from_params).once.times.with(1) }
-        it { is_expected.to have_received(:from_params).once.times.with(2) }
-        it { is_expected.to have_received(:from_params).once.times.with(3) }
+        it { is_expected.to have_received(:from_params).once.with(1) }
+        it { is_expected.to have_received(:from_params).once.with(2) }
+        it { is_expected.to have_received(:from_params).once.with(3) }
       end
 
-      describe 'the relation' do
+      describe 'the relations' do
         subject { relation }
-        it { is_expected.to have_received(:new).exactly(3).times.with({name: nil}) }
+        it { is_expected.to have_received(:new).exactly(3).times.with({name: form_instance.name}) }
       end
 
-      describe 'the new records from the relation' do
+      describe 'the new records from the relations' do
         subject { record }
         it { is_expected.to have_received(:save!).exactly(3).times }
       end
