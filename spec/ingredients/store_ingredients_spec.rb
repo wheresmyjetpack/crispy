1f965a38f7749461f440eac755f4e772a8d7099d
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-19 21:27:30 -0600

    adding Ingredients::StoreIngredients spec

---
+++ spec/ingredients/store_ingredients_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require 'core/ingredients/store_ingredients'
+require 'core/ingredients/ingredient_form'
+
+describe Ingredients::StoreIngredients do
+  let(:form_type) { class_double(Ingredients::IngredientForm, from_params: form_instance) }
+  let(:form_instance) { instance_double(Ingredients::IngredientForm, name: nil) }
+  let(:record) { double('record', save!: nil) }
+  let(:relation) { double('relation', new: record) }
+
+  describe '#call' do
+    before { described_class.call(ingredients, form_type: form_type, relation: relation) }
+
+    context 'when there are three ingredients' do
+      let(:ingredients) { [1, 2, 3] }
+
+      describe 'the form' do
+        subject { form_type }
+        it { is_expected.to have_received(:from_params).once.times.with(1) }
+        it { is_expected.to have_received(:from_params).once.times.with(2) }
+        it { is_expected.to have_received(:from_params).once.times.with(3) }
+      end
+
+      describe 'the relation' do
+        subject { relation }
+        it { is_expected.to have_received(:new).exactly(3).times.with({name: nil}) }
+      end
+
+      describe 'the new records from the relation' do
+        subject { record }
+        it { is_expected.to have_received(:save!).exactly(3).times }
+      end
+    end
+  end
+end
