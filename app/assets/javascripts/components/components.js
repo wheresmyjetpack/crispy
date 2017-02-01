5bc716dcc22abfaae601a5a8def31b90ce854625
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-01-31 19:34:01 -0600

    using generator to build initial shopping items

--- app/assets/javascripts/components/components.js
+++ app/assets/javascripts/components/components.js
@@ -2,6 +2,8 @@
 //= require react_ujs
 //= require components
 
+require ('babel-polyfill');
+
 window.ShoppingList = require('./shopping_list');
 window.FinalizedShoppingList = require('./finalized_shopping_list');
-window.Navbar = require('./navbar')
+window.Navbar = require('./navbar');
--- app/assets/javascripts/components/shopping_list.js.jsx
+++ app/assets/javascripts/components/shopping_list.js.jsx
@@ -4,7 +4,7 @@ class ShoppingListContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputs: [shortid.generate(), shortid.generate(), shortid.generate(), shortid.generate()]
+      inputs: Array.from(initializeInputs(6, this.handleRemove.bind(this)))
     };
   }
 
@@ -12,69 +12,72 @@ class ShoppingListContainer extends React.Component {
     return (
       <ShoppingList
         inputs={this.state.inputs}
-        handleButtonClick={this.handleAdd.bind(this)}
-        handleRemove={this.handleRemove.bind(this)}
-        authenticityToken={this.props.authenticityToken}/>
-    );
+        handleOnClick={this.handleAdd.bind(this)}
+        authToken={this.props.authenticityToken}/>
+      );
   } 
 
   handleAdd() {
-    let newInput = shortid.generate();
+    let elementKey = shortid.generate();
+    let callback = this.handleRemove.bind(this);
     this.setState({
-      inputs: this.state.inputs.concat([newInput])
+      inputs: this.state.inputs.concat([<ShoppingItem key={elementKey} name={elementKey} handleOnClick={callback} />])
     });
   }
 
-  handleRemove(inputName) {
+  handleRemove(element) {
     this.setState({
       inputs: this.state.inputs.filter( (input) => {
-        return input !== inputName;
+        return input.props.name !== element.props.name;
       })
     });
   }
 }
 
-class ShoppingList extends React.Component {
-  render() {
-    return (
-      <div className="p-4 mt-sm-3 mb-sm-4">
-        <form role="form" acceptCharset="UTF-8" action="/shopping_list" method="post" className="p-md-3">
-          <input type="hidden" name="authenticity_token" value={this.props.authenticityToken} />
-          {this.renderInputs()}
-          <hr />
-          <div className="row">
-            <div className="col-lg-4 col-xs-12">
-              <button type="button" className="btn btn-primary mt-sm-2" onClick={ () => this.props.handleButtonClick() }>
-                + Item
-              </button>
-              <input type="submit" className="btn btn-success mt-sm-2" />
-            </div>
+const ShoppingList = ({ inputs, handleOnClick, authToken }) => {
+  return (
+    <div className="p-4 mt-sm-3 mb-sm-4">
+      <form role="form" acceptCharset="UTF-8" action="/shopping_list" method="post" className="p-md-3">
+        <input type="hidden" name="authenticity_token" value={authToken} />
+        {inputs.map((input) => (input))}
+        <hr />
+        <div className="row">
+          <div className="col-lg-4 col-xs-12">
+            <button type="button" className="btn btn-primary mt-sm-2" onClick={ () => handleOnClick() }>
+              + Item
+            </button>
+            <input type="submit" className="btn btn-success mt-sm-2" />
           </div>
-        </form>
-      </div>
-    );
-  }
-
-  renderInputs() {
-    return this.props.inputs.map((inputName) => {
-      return <ShoppingItem key={inputName} name={inputName} handleButtonClick={this.props.handleRemove} />
-    });
-  }
+        </div>
+      </form>
+    </div>
+  );
 }
 
 class ShoppingItem extends React.Component {
   render() {
     return (
-      <div className="row" key={this.props.inputName}>
+      <div className="row">
         <div className="col-lg-6 col-md-8 col-xs-12">
           <div className="shopping-item input-group mr-sm-2">
             <input name="shopping_list[]" className="form-control my-2" />
-            <button type="button" className="btn btn-outline-danger m-2" onClick={ () => this.props.handleButtonClick(this.props.name) }>
-              - Item
+            <button type="button" 
+              className="btn btn-outline-danger m-2" 
+              onClick={ () => this.props.handleOnClick(this) } >
+                - Item
             </button>
           </div>
         </div>
       </div>
+    );
+  }
+}
+
+const initializeInputs = function*(n, callback) {
+  for (let i = 0; i < n; i++) {
+    let elementKey = shortid.generate();
+    yield (
+      <ShoppingItem key={elementKey} name={elementKey} handleOnClick={callback} />
     );
   }
 }
