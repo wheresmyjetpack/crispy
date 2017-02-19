c557eac33a0b340b18c7505fb454b30ca360343e
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-02-19 14:29:44 -0600

    flagging invalid measurements in shopping list component

--- app/assets/javascripts/components/shopping_list.js.jsx
+++ app/assets/javascripts/components/shopping_list.js.jsx
@@ -1,28 +1,54 @@
 const shortid = require('shortid');
+const measurementEndpoint = '/api/v1/measurements.json';
 
 class ShoppingListContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputs: Array.from(initializeInputs(6, this.handleRemove.bind(this)))
+      inputs: Array.from(initializeInputs(6, this.handleBlur.bind(this), this.handleRemove.bind(this))),
+      hasDanger: []
     };
   }
 
   render() {
+    let inputs = this.state.inputs.map( (input) => {
+      if (this.state.hasDanger.includes(input.props.name)) {
+        return this.newInput(
+          input.props.name,
+          input.props.handleOnClick,
+          input.props.handleBlur,
+          true
+        );
+      } else {
+        return input;
+      }
+    });
     return (
       <ShoppingList
-        inputs={this.state.inputs}
+        inputs={inputs}
         handleOnClick={this.handleAdd.bind(this)}
         authToken={this.props.authenticityToken}/>
       );
-  } 
+  }
 
   handleAdd() {
     let elementKey = shortid.generate();
-    let callback = this.handleRemove.bind(this);
+    let onRemove = this.handleRemove.bind(this);
+    let handleOnBlur = this.handleBlur.bind(this);
     this.setState({
-      inputs: this.state.inputs.concat([<ShoppingItem key={elementKey} name={elementKey} handleOnClick={callback} />])
+      inputs: this.state.inputs.concat([this.newInput(elementKey, onRemove, handleOnBlur, false)])
     });
+  }
+
+  newInput(elementKey, onRemove, handleOnBlur, hasDanger) {
+    return (
+    <ShoppingItem
+        key={elementKey}
+        name={elementKey}
+        hasDanger={hasDanger}
+        handleOnClick={onRemove}
+        handleBlur={handleOnBlur}/>
+    );
   }
 
   handleRemove(element) {
@@ -30,6 +56,34 @@ class ShoppingListContainer extends React.Component {
       inputs: this.state.inputs.filter( (input) => {
         return input.props.name !== element.props.name;
       })
+    });
+  }
+
+  handleBlur(context, e) {
+    let amount = e.target.value;
+
+    if (amount === '') {
+      return;
+    }
+
+    $.ajax(measurementEndpoint, {
+      data: {
+        measurement: amount
+      },
+      success: (data) => {
+        if (!data) {
+          this.setState({
+            hasDanger: (this.state.hasDanger.includes(context) ? this.state.hasDanger : this.state.hasDanger.concat(context))
+          });
+        } else {
+          this.setState({
+            hasDanger: this.state.hasDanger.filter( (name) => {
+              return name !== context
+            })
+          });
+        }
+      },
+      error: () => { console.log('AJAX request failed') }
     });
   }
 }
@@ -55,35 +109,42 @@ const ShoppingList = ({ inputs, handleOnClick, authToken }) => {
 }
 
 class ShoppingItem extends React.Component {
+  passEventContext(e) {
+    this.props.handleBlur(this.props.name, e)
+  }
+
   render() {
     let inputName = `shopping_list[${this.props.name}][name]`;
     let inputAmt = `shopping_list[${this.props.name}][amount]`;
+    let amtClasses = 'form-control';
+    let amtWrapperClasses = 'form-group col-sm-2';
 
-    return (
-      <div className="row form-group">
-        <div className="col-sm-6">
-          <input name={inputName} className="form-control" />
-        </div>
-        <div className="col-sm-2">
-          <input name={inputAmt} className="form-control" type="text" />
-        </div>
-        <div className="col-sm-4">
-          <button type="button"
-            className="btn btn-outline-danger"
-            onClick={ () => this.props.handleOnClick(this) } >
-            - Item
-          </button>
-        </div>
+    if (this.props.hasDanger) {
+      amtClasses = 'form-control form-control-danger';
+      amtWrapperClasses = 'form-group has-danger col-sm-2';
+    }
+
+    return (<div className="row form-group">
+      <div className="col-sm-6">
+        <input name={inputName} className="form-control" />
       </div>
-      );
+      <div className={amtWrapperClasses}>
+        <input name={inputAmt} className={amtClasses} type="text" onBlur={ (e) => this.passEventContext(e) } />
+      </div>
+      <div className="col-sm-4">
+        <button type="button"
+          className="btn btn-outline-danger"
+          onClick={ () => this.props.handleOnClick(this) } > - Item</button>
+      </div>
+    </div>);
   }
 }
 
-const initializeInputs = function*(n, callback) {
+const initializeInputs = function*(n, handleOnBlur, handleClick) {
   for (let i = 0; i < n; i++) {
     let elementKey = shortid.generate();
     yield (
-      <ShoppingItem key={elementKey} name={elementKey} handleOnClick={callback} />
+      <ShoppingItem hasDanger={false} key={elementKey} name={elementKey} handleBlur={handleOnBlur} handleOnClick={handleClick} />
       );
   }
 }
