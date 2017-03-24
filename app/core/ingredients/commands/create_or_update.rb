8332e33af33423c148a4960dc5522d56652a61bb
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-03-23 23:03:11 -0500

    inheriting from base Update class in Ingredients::Commands::CreateOrUpdate

--- app/core/ingredients/commands/create_or_update.rb
+++ app/core/ingredients/commands/create_or_update.rb
@@ -1,13 +1,11 @@
 module Ingredients
   module Commands
-    class CreateOrUpdate
-      include Transproc::Composer
-      cattr_reader :by_key
+    class CreateOrUpdate < Persistence::Commands::Update
+      chain_ops update: [:calc_measurements, :update_measurements]
 
-      def initialize(record, model, **opts)
+      def initialize(record, model: Ingredient)
         @record = record
         @model = model
-        @opts = opts
       end
 
       def call
@@ -16,57 +14,38 @@ module Ingredients
         update.call(existing: query, new: record)
       end
 
-      def self.call(record, model= Ingredient, **opts)
-        raise RelationKeyError, "The lookup key has not been defined by. Do so with #{self}.with_key" if by_key.nil?
-        new(record, model, opts).call
-      end
-
-      def self.with_key(pairing)
-        @@by_key = pairing
-        self
-      end
-
       private
 
-      attr_reader :record, :model, :opts
+      attr_reader :record, :model
 
       def lookup
-        t(:lookup)
+        op(:lookup)
       end
 
       def create
-        t(:create)
-      end
-
-      def update
-        [calc_measurements, update_measurements].reduce(:>>)
+        op(:create)
       end
 
       def update_measurements
         compose do |ops|
-          ops << t(:map_value_to_hash, :new)
-          ops << t(-> h { h.merge(new: h[:new].merge(h[:updated_measure]) )})
-          ops << t(:nest, :args, [:existing, :new])
-          ops << t(:map_value, :args, -> h { h[:existing].update(h[:new]) })
-          ops << t(:rename_keys, args: :success)
+          ops << op(:map_value_to_hash, :new)
+          ops << op(-> h { h.merge(new: h[:new].merge(h[:updated_measure]) )})
+          ops << op(:nest, :args, [:existing, :new])
+          ops << op(:map_value, :args, -> h { h[:existing].update(h[:new]) })
+          ops << op(:rename_keys, args: :success)
         end
       end
 
       def calc_measurements
         compose do |ops|
-          ops << t(:copy_keys, {existing: :existing_measure, new: :new_measure})
-          ops << t(:map_value, :existing_measure, t(:collapse_measurement))
-          ops << t(:map_value, :new_measure, t(:collapse_measurement))
-          ops << t(-> h { h.merge(updated_measure: h[:new_measure] + h[:existing_measure]) })
-          ops << t(:map_value, :updated_measure, t(:explode_measurement))
-          ops << t(:reject_keys, [:existing_measure, :new_measure])
+          ops << op(:copy_keys, {existing: :existing_measure, new: :new_measure})
+          ops << op(:map_value, :existing_measure, op(:collapse_measurement))
+          ops << op(:map_value, :new_measure, op(:collapse_measurement))
+          ops << op(-> h { h.merge(updated_measure: h[:new_measure] + h[:existing_measure]) })
+          ops << op(:map_value, :updated_measure, op(:explode_measurement))
+          ops << op(:reject_keys, [:existing_measure, :new_measure])
         end
       end
-
-      def t(*args)
-        Operations[*args]
-      end
-      class RelationKeyError < StandardError; end
     end
   end
 end
