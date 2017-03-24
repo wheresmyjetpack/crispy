d9c9c996b007ca2fb3f5aed060df82f2a2935b00
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-03-23 22:07:19 -0500

    adding Ingredients::Commands::CreateOrUpdate

--- app/core/ingredients/commands/create_or_update.rb
+++ app/core/ingredients/commands/create_or_update.rb
@@ -2,8 +2,7 @@ module Ingredients
   module Commands
     class CreateOrUpdate
       include Transproc::Composer
-
-      Functions.register(:collapse_measurement) { |v| "#{v.amount} #{v.unit}".to_measurement }
+      cattr_reader :by_key
 
       def initialize(record, model, **opts)
         @record = record
@@ -12,13 +11,19 @@ module Ingredients
       end
 
       def call
-        query = lookup.call(model: model, opts: opts)
+        query = lookup.call(model: model, by_key: by_key)
         return create.call(model: model, record: record) if query.nil?
         update.call(existing: query, new: record)
       end
 
       def self.call(record, model= Ingredient, **opts)
+        raise RelationKeyError, "The lookup key has not been defined by. Do so with #{self}.with_key" if by_key.nil?
         new(record, model, opts).call
+      end
+
+      def self.with_key(pairing)
+        @@by_key = pairing
+        self
       end
 
       private
@@ -26,30 +31,42 @@ module Ingredients
       attr_reader :record, :model, :opts
 
       def lookup
-        t(-> h { h[:model].find_by(**h[:opts]) })
+        t(:lookup)
       end
 
       def create
-        t(-> h { h[:model].create(**h[:record]) })
+        t(:create)
       end
 
       def update
-        compose do |fns|
-          fns << t(:copy_keys, {existing: :existing_measure, new: :new_measure})
-          fns << t(:map_value, :existing_measure, t(:collapse_measurement))
-          fns << t(:map_value, :new_measure, t(:collapse_measurement))
-          fns << t(-> h { h[:updated_measure] = h[:new_measure] + h[:existing_measure]; h })
-          fns << t(:map_value, :new, -> n { n.to_h })
-          fns << t(-> h { h[:new] = h[:new].merge(amount: h[:updated_measure].quantity, unit: h[:updated_measure].unit); h })
-          fns << t(:nest, :args, [:existing, :new])
-          fns << t(:map_value, :args, -> h { h[:existing].update(h[:new]) })
-          fns << t(:rename_keys, args: :success)
+        [calc_measurements, update_measurements].reduce(:>>)
+      end
+
+      def update_measurements
+        compose do |ops|
+          ops << t(:map_value_to_hash, :new)
+          ops << t(-> h { h.merge(new: h[:new].merge(h[:updated_measure]) )})
+          ops << t(:nest, :args, [:existing, :new])
+          ops << t(:map_value, :args, -> h { h[:existing].update(h[:new]) })
+          ops << t(:rename_keys, args: :success)
+        end
+      end
+
+      def calc_measurements
+        compose do |ops|
+          ops << t(:copy_keys, {existing: :existing_measure, new: :new_measure})
+          ops << t(:map_value, :existing_measure, t(:collapse_measurement))
+          ops << t(:map_value, :new_measure, t(:collapse_measurement))
+          ops << t(-> h { h.merge(updated_measure: h[:new_measure] + h[:existing_measure]) })
+          ops << t(:map_value, :updated_measure, t(:explode_measurement))
+          ops << t(:reject_keys, [:existing_measure, :new_measure])
         end
       end
 
       def t(*args)
-        Functions[*args]
+        Operations[*args]
       end
+      class RelationKeyError < StandardError; end
     end
   end
 end
