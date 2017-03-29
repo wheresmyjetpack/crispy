2de74bba84d7982c75e87e6a445748b4a62d863d
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-03-28 21:26:23 -0500

    changing user password length requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 10 }
 end
