04815e3e0d15cbf21320457d3739e4f6da0105bf
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-04-05 19:14:28 -0500

    removing Command abstraction out from Persistence::Commands::Update

--- lib/persistence/commands/update.rb
+++ lib/persistence/commands/update.rb
@@ -1,33 +1,9 @@
 module Persistence
   module Commands
     class Update
-      include Transproc::Composer
+      include Command
 
       cattr_reader :by_key
-
-      def op(*args)
-        Operations[*args]
-      end
-
-      def operation(&block)
-        compose(&block)
-      end
-
-      def self.chain_ops(ops)
-        ops.each do |name, op_list|
-          define_method(name) do
-            op_list.map { |op| send(op) }.reduce(:>>)
-          end
-        end
-      end
-
-      def self.operations(*ops)
-        ops.each do |o|
-          define_method(o) do
-            op(o)
-          end
-        end
-      end
 
       def self.call(record)
         raise Persistence::Errors::LookupKeyError, "The lookup key has not been defined. Do so with #{self}.with_key" if by_key.nil?
