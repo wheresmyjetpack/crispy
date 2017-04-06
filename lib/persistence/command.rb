06d082186d7ad3f9b96e6b8868cc954bf49418a3
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-04-05 19:01:48 -0500

    adding Persistence::Command abstract class

---
+++ lib/persistence/command.rb
@@ -0,0 +1,35 @@
+module Persistence
+  module Command
+    extend ActiveSupport::Concern
+
+    def op(*args)
+      Operations[*args]
+    end
+
+    def operation(&block)
+      compose(&block)
+    end
+
+    included do
+      include Transproc::Composer
+    end
+
+    class_methods do
+      def chain_ops(ops)
+        ops.each do |name, op_list|
+          define_method(name) do
+            op_list.map { |op| send(op) }.reduce(:>>)
+          end
+        end
+      end
+
+      def operations(*ops)
+        ops.each do |o|
+          define_method(o) do
+            op(o)
+          end
+        end
+      end
+    end
+  end
+end
