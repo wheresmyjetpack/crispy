80eb004767297d623f344234cab6f1ab16e41352
wheresmyjetpack <wheresmyjetpack03@gmail.com>
2017-04-05 19:13:12 -0500

    adding email type

--- app/core/types.rb
+++ app/core/types.rb
@@ -2,4 +2,5 @@ module Types
   include Dry::Types.module
 
   ItemMeasurement = String.constrained(format: /(\d+|\d+\.?\d+) \w+\.?$/)
+  Email = String.constrained(format: /[\w\d\-_.]+?@[a-z\d\-.]+?(?<!\.)\.[a-z]+/i)
 end
